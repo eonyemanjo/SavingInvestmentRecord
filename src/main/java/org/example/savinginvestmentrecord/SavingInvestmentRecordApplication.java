3e7d862e7b72ba8726ad4d3249ae87210f83e679
Ekene Innocent Onyemanjo <innoscisco@yahoo.com>
2024-08-12 13:21:18 -0700

    First Commit

---
+++ src/main/java/org/example/savinginvestmentrecord/SavingInvestmentRecordApplication.java
@@ -0,0 +1,13 @@
+package org.example.savinginvestmentrecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavingInvestmentRecordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SavingInvestmentRecordApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/savinginvestmentrecord/SavingInvestmentRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.savinginvestmentrecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavingInvestmentRecordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
